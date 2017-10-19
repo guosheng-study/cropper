4a1323d77ee9298a84c4008fb4b553dff092d0c7
Guo <kyo.guo@capgemini.com>
2017-10-19 13:17:06 +0800

    cropper

--- index.js
+++ index.js
@@ -25,10 +25,36 @@ $('#image').on('load', function() {
     ready: function() {
       var self = $(this);
       self.cropper('zoomTo', 1);
+      jRange.setValue('1.00');
+
+      cropper.on('zoom', function(e) {
+        jRange.setValue(String(e.ratio));
+      });
+
+    },
+    zoom: function(e) {
+      if (e.ratio > 1.5 || e.ratio < 0.5) {
+        e.preventDefault();
+      }
     },
     crop: function(e) {
 
     }
   });
+
+  var jRange = $('.single-slider').jRange({
+    from: 0.5,
+    to: 1.5,
+    step: 0.1,
+    width: 300,
+    theme: 'theme-blue',
+    showLabels: false,
+    snap: true,
+    onstatechange: function(res) {
+      cropper.cropper('zoomTo', res);
+    }
+  });
+
+
 
 });
